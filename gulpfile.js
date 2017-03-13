acb86aa5fd5e3944fe5b0c68e611a8513d05c4f4
afonsopacifer <afonsopolis@gmail.com>
2017-03-12 21:08:48 -0300

    Servidor estatico com livereload adicionado

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp')
 var pug = require('gulp-pug')
 var stylus = require('gulp-stylus')
+var connect = require('gulp-connect')
 
 gulp.task('pug', function() {
     gulp.src('./src/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('./out'))
+        .pipe(connect.reload())
 })
 
 gulp.task('stylus', function() {
     gulp.src('./src/assets/styles/*.styl')
         .pipe(stylus())
         .pipe(gulp.dest('./out/assets/styles/'))
+        .pipe(connect.reload())
 })
 
 gulp.task('watch', function() {
@@ -19,4 +22,12 @@ gulp.task('watch', function() {
     gulp.watch(['./src/assets/styles/*.styl'],['stylus'])
 })
 
+gulp.task('serve', function() {
+    connect.server({
+        root: './out',
+        livereload: true
+    })
+})
+
 gulp.task('build', ['pug','stylus'])
+gulp.task('server', ['serve','watch'])
